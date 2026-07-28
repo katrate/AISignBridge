61f91b0754e513822d9a22b43bf85448a66c8ee8
katrate <katrate@users.noreply.github.com>
2026-07-28 15:06:37 +0530

    Fix smooth scroll handler crashing on download button URLs

--- website/js/main.js
+++ website/js/main.js
@@ -474,8 +474,10 @@
   function initSmoothScroll() {
     document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
       anchor.addEventListener('click', function (e) {
+        var href = this.getAttribute('href');
+        if (!href || href === '#' || href.indexOf('#') !== 0) return;
         e.preventDefault();
-        var target = document.querySelector(this.getAttribute('href'));
+        var target = document.querySelector(href);
         if (target) {
           target.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
